8b6468e66b1fa4e551018e4cc8c1cc05d849b4ab
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2025-11-21 11:15:45 +0100

    New translations mudlet.ts (Portuguese, Brazilian)

--- translations/translated/mudlet_pt_BR.ts
+++ translations/translated/mudlet_pt_BR.ts
@@ -1474,7 +1474,7 @@ Accessibility-friendly description for the built-in command line of a console/wi
       <location filename="../src/TConsole.cpp" line="357"/>
       <source>Start logging game output to log file.</source>
       <extracomment>Button tooltip for the logging button</extracomment>
-      <translation type="unfinished"/>
+      <translation>Comece a registrar a saída do jogo no arquivo de log.</translation>
     </message>
     <message>
       <location filename="../src/TConsole.cpp" line="435"/>
@@ -4030,7 +4030,7 @@ area) not found, adding &quot;%1&quot; against the reserved -1 id.</source>
     <message>
       <location filename="../src/TTextEdit.cpp" line="1962"/>
       <source>Unknown</source>
-      <translation type="unfinished"/>
+      <translation>Desconhecido</translation>
     </message>
     <message>
       <location filename="../src/TTextEdit.cpp" line="1963"/>
